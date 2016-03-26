6250d019da41e572e5360fa2ff5d30d6f7f4c19c
Damith Hettige <damithhettige@Damith-Hettige.local>
2016-03-27 06:58:06 +0800

    new project

---
+++ SwiftTutorials/SwiftTutorials/Source/Optionals.swift
@@ -0,0 +1,13 @@
+//
+//  Optionals.swift
+//  SwiftTutorials
+//
+//  Created by Damith Hettige on 23/3/16.
+//  Copyright © 2016 Damith Hettige. All rights reserved.
+//
+
+import UIKit
+
+class Optionals: NSObject {
+
+}
---
+++ SwiftTutorials/SwiftTutorials/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftTutorials
+//
+//  Created by Damith Hettige on 23/3/16.
+//  Copyright © 2016 Damith Hettige. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
